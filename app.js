4f4360af257e26d6f6b55f4414647c047e1395cb
marimeli <marimel.casas@gmail.com>
2019-10-11 12:07:02 -0500

    forbidden status in posts routes

--- app.js
+++ app.js
@@ -2,12 +2,46 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ message: ' Welcome to the API' });
 });
 
+//create a route that I ewant to protect. Add middleware (verifytoken)
+app.post('/api/posts', verifyToken, (req, res) => {
+  res.json({ message: 'Post created' })
+});
 
-const PORT = 5000; 
+app.post('/api/login', (req, res) => {
+  //Mock user
+  const user = {
+    id: 1,
+    username: 'meli',
+    email: 'meli@gmail.com'
+  }
+
+  jwt.sign({ user }, 'secretKey', (err, token) => {
+    res.json({ token })
+  }); //async, send it along the payload (3 parametros)
+});
+
+// Format of token
+//Authorization: Bearer<access_token>
+
+//Verify token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Ckeck if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    return 'hola'
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+};
+
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
